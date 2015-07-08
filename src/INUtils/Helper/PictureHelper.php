a50eb34e98e74e3f8dad158f90df7ce53b8c14f0
Javier De la Hoz <javierdlahoz@gmail.com>
2015-07-07 21:44:35 -0500

    picture helper

--- src/INUtils/Helper/PictureHelper.php
+++ src/INUtils/Helper/PictureHelper.php
@@ -11,23 +11,28 @@ class PictureHelper
     public static function getThumbnail($url, $size = 25) {
 
         $fileName = self::getImageName($url);
-        if(exif_imagetype($url) != IMAGETYPE_JPEG){
-            list($widthOrig, $heightOrig) = getimagesize($url);
-
-            if($heightOrig > 840){
-                $width = ceil($widthOrig*$size/100);
-                $height = ceil($heightOrig*$size/100);
-
-                // This resamples the image
-                $imageR = \imagecreatetruecolor($width, $height);
-                $image = \imagecreatefromjpeg($url);
-                \imagecopyresampled($imageR, $image, 0, 0, 0, 0, $width, $height, $widthOrig, $heightOrig);
-
-                $destinationPath = __DIR__.self::PATH.$fileName;
-                if(!file_exists($destinationPath)){
-                    imagejpeg($imageR, $destinationPath);
+        if(!empty($url)){
+            if(exif_imagetype($url) != IMAGETYPE_JPEG){
+                list($widthOrig, $heightOrig) = getimagesize($url);
+            
+                if($heightOrig > 840){
+                    $width = ceil($widthOrig*$size/100);
+                    $height = ceil($heightOrig*$size/100);
+            
+                    // This resamples the image
+                    $imageR = \imagecreatetruecolor($width, $height);
+                    $image = \imagecreatefromjpeg($url);
+                    \imagecopyresampled($imageR, $image, 0, 0, 0, 0, $width, $height, $widthOrig, $heightOrig);
+            
+                    $destinationPath = __DIR__.self::PATH.$fileName;
+                    if(!file_exists($destinationPath)){
+                        imagejpeg($imageR, $destinationPath);
+                    }
+                    return self::PATH_TO_HERE.self::PATH.$fileName;
                 }
-                return self::PATH_TO_HERE.self::PATH.$fileName;
+                else{
+                    return $url;
+                }
             }
             else{
                 return $url;
